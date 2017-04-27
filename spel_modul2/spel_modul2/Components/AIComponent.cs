a21a88f7643ae74b35218d2dacc88b5e4b97376b
slayme89 <rob.gustavsson@hotmail.com>
2017-04-27 11:19:54 +0200

    AI

---
+++ spel_modul2/spel_modul2/Components/AIComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace GameEngine
+{
+    class AIComponent : IComponent
+    {
+        public Point Destination { get; set; }
+    }
+}
---
+++ spel_modul2/spel_modul2/Systems/AISystem.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace GameEngine
+{
+    class AISystem : ISystem
+    {
+
+
+        public void Update(GameTime gameTime)
+        {
+            var ai = ComponentManager.GetInstance().GetComponentsOfType<AIComponent>();
+
+
+            foreach(KeyValuePair<int, IComponent> pair in ai)
+            {
+               PositionComponent positionComp = ComponentManager.GetInstance().GetComponentForEntity<PositionComponent>(pair.Key);
+               if(positionComp != null)
+                {
+
+                }
+            }
+
+        }
+    }
+}
