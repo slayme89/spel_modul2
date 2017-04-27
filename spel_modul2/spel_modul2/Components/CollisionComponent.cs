e8742cc40cd6ff09cbb53521e1359d72f1e46c82
slayme89 <rob.gustavsson@hotmail.com>
2017-04-27 22:05:42 +0200

    Added collisionComponent

---
+++ spel_modul2/spel_modul2/Components/CollisionComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace GameEngine
+{
+    class CollisionComponent
+    {
+        public Rectangle collisionBox;
+
+        public CollisionComponent(int height, int width, Point position)
+        {
+            collisionBox.Height = height;
+            collisionBox.Width = width;
+            collisionBox.Location = position;
+        }
+    }
+}
