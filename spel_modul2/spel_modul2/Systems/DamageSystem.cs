0d7477e021ac474c24d3c3e52c566c0ade7d2368
slayme89 <rob.gustavsson@hotmail.com>
2017-05-01 12:39:45 +0200

    Added bool in HealthComponent. Added DamageSystem

---
+++ spel_modul2/spel_modul2/Systems/DamageSystem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace GameEngine
+{
+    class DamageSystem : ISystem
+    {
+        public void Update(GameTime gameTime)
+        {
+        }
+
+        public void Update(GameTime gameTime, int entityHit, int attackingEntity)
+        {
+            ApplyDamageToEntity(entityHit, attackingEntity);
+        }
+
+        private void ApplyDamageToEntity(int entityHit, int attackingEntity)
+        {
+            ComponentManager cm = ComponentManager.GetInstance();
+            HealthComponent entityHitHealth = cm.GetComponentForEntity<HealthComponent>(entityHit);
+            AttackComponent attackingEntityDamage = cm.GetComponentForEntity<AttackComponent>(attackingEntity);
+
+            entityHitHealth.Current -= attackingEntityDamage.Damage;
+            if(entityHitHealth.Current <= 0)
+            {
+                entityHitHealth.IsAlive = false;
+            } 
+        }
+    }
+}
