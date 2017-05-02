328399ad88f2b3ef8a5382ae13412cd96729086a
Fricken <victor.j.frick@gmail.com>
2017-05-02 15:01:57 +0200

    Added interact system anc component

---
+++ spel_modul2/spel_modul2/Systems/InteractSystem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace GameEngine
+{
+    class InteractSystem : ISystem
+    {
+        public void Update(GameTime gameTime)
+        {
+            ComponentManager cm = ComponentManager.GetInstance();
+            foreach (var player in cm.GetComponentsOfType<PlayerComponent>())
+            {
+                PlayerComponent playerComponent = (PlayerComponent)player.Value;
+                PlayerControlComponent controlComponent = cm.GetComponentForEntity<PlayerControlComponent>(player.Key);
+                
+                PositionComponent playerPositionComponent = cm.GetComponentForEntity<PositionComponent>(player.Key);
+                int closestInteractable = FindClosestInteractable(playerPositionComponent.position);
+                if(closestInteractable != -1)
+                {
+                    if (controlComponent.Interact.IsButtonDown())
+                    {
+                        // Interact with closest interactable entity
+                        // Temp
+                        SystemManager.GetInstance().GetSystem<DamageSystem>().Update(player.Key, closestInteractable);
+                    }
+                }
+
+                
+            }
+        }
+
+        private int FindClosestInteractable(Point position)
+        {
+            ComponentManager cm = ComponentManager.GetInstance();
+            int closestNr = -1;
+            float closestDist = 100;
+            foreach (var entity in cm.GetComponentsOfType<InteractComponent>())
+            {
+                PositionComponent positionComponent = cm.GetComponentForEntity<PositionComponent>(entity.Key);
+                float distance = CalcDistance(position, positionComponent.position);
+                if(distance < closestDist)
+                {
+                    closestNr = entity.Key;
+                    closestDist = distance;
+                }
+            }
+            return closestNr;
+        }
+
+        private float CalcDistance(Point destination, Point origin)
+        {
+            int x = destination.X - origin.X;
+            int y = destination.Y - origin.Y;
+            return (float)Math.Sqrt(x * x + y * y);
+        }
+    }
+}
