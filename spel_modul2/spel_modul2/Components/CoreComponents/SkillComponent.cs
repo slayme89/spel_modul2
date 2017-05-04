c548980fa5bba6d72a9e7fa098edd9eb0bcf28d6
Kristoffer . <sony_datf@hotmail.com>
2017-05-04 12:53:05 +0200

    Created SkillComponent and SkillSystem

---
+++ spel_modul2/spel_modul2/Components/CoreComponents/SkillComponent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    public enum SkillType
+    {
+        HeavyAttack,
+        QuickAttack,
+        FireBall
+    }
+
+    class SkillComponent : IComponent
+    {
+        public float CooldownTimer { get; set; }
+        public float Cooldown { get; set; }
+        public SkillType SkillType { get; set; }
+        public int EnergyCost { get; set; }
+
+        public SkillComponent(int cooldown, SkillType skillType, int energyCost)
+        {
+            Cooldown = cooldown;
+            CooldownTimer = 0;
+            SkillType = skillType;
+            EnergyCost = energyCost;
+        }
+    }
+}
---
+++ spel_modul2/spel_modul2/Systems/PlayerSystems/SkillSystem.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace GameEngine
+{
+    class SkillSystem : ISystem
+    {
+        public void Update(GameTime gameTime)
+        {
+            ComponentManager cm = ComponentManager.GetInstance();
+            foreach (var Entity in cm.GetComponentsOfType<SkillComponent>())
+            {
+                SkillComponent skillComponenet = cm.GetComponentForEntity<SkillComponent>(Entity.Key);
+                AttackComponent attackComponent = cm.GetComponentForEntity<AttackComponent>(Entity.Key);
+                EnergyComponent energyComponent = cm.GetComponentForEntity<EnergyComponent>(Entity.Key);
+                if(skillComponenet.SkillType == SkillType.HeavyAttack)
+                {
+                    if(skillComponenet.CooldownTimer <= 0 && skillComponenet.EnergyCost < energyComponent.Current)
+                    {
+                        attackComponent.Damage = attackComponent.Damage * 2;
+                        energyComponent.Current = skillComponenet.EnergyCost;
+                        skillComponenet.CooldownTimer = skillComponenet.Cooldown; 
+                    }
+                    else
+                    {
+                        skillComponenet.CooldownTimer -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    }
+                }
+            }
+        }
+    }
+}
