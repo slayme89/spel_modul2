858655e8551105b8b4d1f549e9beaa26bd997419
Fricken <victor.j.frick@gmail.com>
2017-05-16 12:25:10 +0200

    Fixed so that the player can't move during attacks

--- spel_modul2/spel_modul2/Systems/PlayerSystems/PlayerMovementSystem.cs
+++ spel_modul2/spel_modul2/Systems/PlayerSystems/PlayerMovementSystem.cs
@@ -12,14 +12,18 @@ namespace GameEngine
             {
                 PlayerControlComponent playerControlComponent = (PlayerControlComponent)entity.Value;
                 MoveComponent moveComponent = cm.GetComponentForEntity<MoveComponent>(entity.Key);
-                moveComponent.Velocity = playerControlComponent.Movement.GetDirection();
-                if(playerControlComponent.Movement.GetDirection() != new Vector2(0.0f, 0.0f))
+                if (moveComponent.canMove)
                 {
-                    if (cm.HasEntityComponent<SoundComponent>(entity.Key))
+                    moveComponent.Velocity = playerControlComponent.Movement.GetDirection();
+                    if (playerControlComponent.Movement.GetDirection() != new Vector2(0.0f, 0.0f))
                     {
-                        cm.GetComponentForEntity<SoundComponent>(entity.Key).PlayWalkSound = true;
+                        if (cm.HasEntityComponent<SoundComponent>(entity.Key))
+                        {
+                            cm.GetComponentForEntity<SoundComponent>(entity.Key).PlayWalkSound = true;
+                        }
                     }
                 }
+                
             }
         }
     }
