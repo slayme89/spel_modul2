04e76d93bf134ad0e0983f50eeb79b5724bd6b68
slayme89 <rob.gustavsson@hotmail.com>
2017-05-16 14:33:19 +0200

    Menusystem fixed

---
+++ spel_modul2/spel_modul2/Systems/MenuSystem/MenuSystem.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace GameEngine
+{
+    class MenuSystem : ISystem
+    {
+        public void Update(GameTime gameTime)
+        {
+            //If the state is set to menu
+            if(StateManager.GetInstance().GetState() == "menu")
+            {
+
+
+            }
+
+
+            //If the state is set to game-mode
+            if (StateManager.GetInstance().GetState() == "game")
+            {
+
+
+            }
+        }
+    }
+}
