dd869c64a4d7a580d934c230bd5009bfecb0f6c8
flippy84 <ante84@gmail.com>
2017-05-18 12:23:22 +0200

    Project rename

--- spel_modul2/GameEngine/Properties/AssemblyInfo.cs
+++ spel_modul2/GameEngine/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("spel_modul2")]
-[assembly: AssemblyProduct("spel_modul2")]
+[assembly: AssemblyTitle("GameEngine")]
+[assembly: AssemblyProduct("GameEngine")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
