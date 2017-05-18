7ea500bec1ac3ee34222b298bd37d3f44e89aa50
slayme89 <rob.gustavsson@hotmail.com>
2017-05-18 11:48:15 +0200

    Quest stuff

---
+++ spel_modul2/spel_modul2/Components/CoreComponents/QuestComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace GameEngine
+{
+    class QuestComponent : IComponent
+    {
+        public string Text { get; set; }
+        public bool IsDone { get; set; }
+        public bool IsStarted { get; set; }
+        public int Experience { get; set; }
+    }
+}
---
+++ spel_modul2/spel_modul2/Systems/CoreSystems/QuestSystem.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace GameEngine
+{
+    class QuestSystem : ISystem
+    {
+        public void Update(GameTime gameTime)
+        {
+            
+        }
+    }
+}
