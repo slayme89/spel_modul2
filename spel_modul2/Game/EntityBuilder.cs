4b2346384d15d67eb7c1ed56ec43189795450da9
flippy84 <ante84@gmail.com>
2017-05-18 12:09:52 +0200

    New factory and builder

---
+++ spel_modul2/Game/EntityBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using GameEngine.Components;
+
+namespace Game
+{
+    public class EntityBuilder
+    {
+        List<IComponent> components;
+
+        public EntityBuilder()
+        {
+            components = new List<IComponent>();
+        }
+
+        public EntityBuilder SetPositon(int x, int y)
+        {
+            components.Add(new PositionComponent(x, y));
+            return this;
+        }
+
+        public IComponent[] Build()
+        {
+            return components.ToArray();
+        }
+    }
+}
---
+++ spel_modul2/Game/EntityFactory.cs
@@ -0,0 +1,59 @@
+﻿using GameEngine.Components;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace Game
+{
+    public class EntityFactory
+    {
+        public IComponent[] CreateEnemy(int x, int y)
+        {
+            return new IComponent[]
+            {
+                new AnimationComponent("PlayerAnimation/NakedFWalk", new Point(4, 1), 150),
+                new HealthComponent(50),
+                new PositionComponent(x, y),
+                new MoveComponent(0.1f),
+                new AIComponent(160, 160, false),
+                new CollisionComponent(50, 50),
+                new SoundComponent("Sound/walk", "Sound/sword", "Sound/damage"),
+                new AttackComponent(10, 0.5f, 0.3f, WeaponType.Sword),
+                new LevelComponent(5),
+                new DamageComponent(),
+                new KnockbackComponent(),
+            };
+        }
+
+        public IComponent[] CreatePlayer(int x, int y)
+        {
+            return new IComponent[]
+            {
+                new AnimationGroupComponent("PlayerSpritesheet", new Point(4, 4), 150,
+                new[] {
+                    new Tuple<Point, Point>(new Point(0, 0), new Point(1, 1)),
+                    new Tuple<Point, Point>(new Point(0, 0), new Point(4, 1)),
+                    new Tuple<Point, Point>(new Point(0, 1), new Point(4, 1)),
+                    new Tuple<Point, Point>(new Point(0, 2), new Point(4, 1)),
+                    new Tuple<Point, Point>(new Point(0, 3), new Point(4, 1)),
+                }),
+                new HealthComponent(100),
+                new PositionComponent(x, y),
+                new MoveComponent(0.2f),
+                new PlayerControlComponent(ControllerType.Keyboard),
+                new CollisionComponent(50, 50),
+                new AttackComponent(10, 0.3f, 0.1f, WeaponType.Sword),
+                new PlayerComponent(1),
+                new LevelComponent(1, 80),
+                new SoundComponent("Sound/walk", "Sound/sword", "Sound/damage"),
+                new ActionBarComponent(),
+                new GUIComponent("UI/Player1-Hp-Ene-Xp", GUIPosition.Left),
+                new InventoryComponent(5, 4),
+                new EnergyComponent(100),
+                new DamageComponent(),
+                new StatsComponent(5, 1, 0, 0),
+                new KnockbackComponent(),
+            };
+        }
+    }
+}
