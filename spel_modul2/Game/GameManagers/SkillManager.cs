62929281180a32edf06d5a5ae578c751f7b03c38
flippy84 <ante84@gmail.com>
2017-05-21 15:55:51 +0200

    Changed method used and null check

--- spel_modul2/Game/GameManagers/SkillManager.cs
+++ spel_modul2/Game/GameManagers/SkillManager.cs
@@ -18,26 +18,30 @@ namespace GameEngine.Managers
         public static void HeavyAttack(int entity)
         {
             ComponentManager cm = ComponentManager.GetInstance();
-            PositionComponent posComp = cm.GetComponentForEntity<PositionComponent>(entity);
-            MoveComponent moveComp = cm.GetComponentForEntity<MoveComponent>(entity);
-            CollisionComponent collComp = cm.GetComponentForEntity<CollisionComponent>(entity);
-            StatsComponent statComp = cm.GetComponentForEntity<StatsComponent>(entity);
-            int width = 70;
-            int height = 70;
-            Point attackPoint = new Point(moveComp.Direction.X * ((width/2) + (collComp.CollisionBox.Size.X/2)) - (width/2), moveComp.Direction.Y * ((height/2) + (collComp.CollisionBox.Size.Y/2)) - (height/2));
-            Rectangle areOfEffect = new Rectangle(posComp.Position.ToPoint() + attackPoint , new Point(width, height));
-            heavyAttack = areOfEffect;
-            List<int> entitiesHit = CollisionSystem.DetectAreaCollision(areOfEffect);
-            foreach(int entityHit in entitiesHit)
+            PositionComponent posComp;
+            MoveComponent moveComp;
+            CollisionComponent collComp;
+            StatsComponent statComp;
+            if (cm.TryGetEntityComponents(entity, out posComp, out moveComp, out collComp, out statComp))
             {
-                if (entityHit == entity)
-                    continue;
-                if (cm.HasEntityComponent<DamageComponent>(entityHit) && !cm.HasEntityComponent<PlayerControlComponent>(entityHit))
+
+                int width = 70;
+                int height = 70;
+                Point attackPoint = new Point(moveComp.Direction.X * ((width / 2) + (collComp.CollisionBox.Size.X / 2)) - (width / 2), moveComp.Direction.Y * ((height / 2) + (collComp.CollisionBox.Size.Y / 2)) - (height / 2));
+                Rectangle areOfEffect = new Rectangle(posComp.Position.ToPoint() + attackPoint, new Point(width, height));
+                heavyAttack = areOfEffect;
+                List<int> entitiesHit = CollisionSystem.DetectAreaCollision(areOfEffect);
+                foreach (int entityHit in entitiesHit)
                 {
-                    DamageComponent damageComp = cm.GetComponentForEntity<DamageComponent>(entityHit);
-                    float damage = 11 + statComp.Strength * 3.5f;
-                    damageComp.IncomingDamage.Add((int)damage);
-                    damageComp.LastAttacker = entity;
+                    if (entityHit == entity)
+                        continue;
+                    if (cm.HasEntityComponent<DamageComponent>(entityHit) && !cm.HasEntityComponent<PlayerControlComponent>(entityHit))
+                    {
+                        DamageComponent damageComp = cm.GetComponentForEntity<DamageComponent>(entityHit);
+                        float damage = 11 + statComp.Strength * 3.5f;
+                        damageComp.IncomingDamage.Add((int)damage);
+                        damageComp.LastAttacker = entity;
+                    }
                 }
             }
         }
